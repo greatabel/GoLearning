eed28cb42e1bdf8bf0a5e85fea8a176ddac3de5b
greatabel <myreceiver2for2github@gmail.com>
2019-01-07 22:39:53 +0800

    4leap.go

---
+++ 00Get Programming with Go/03LOOPS_AND_BRANCHES/4leap.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    var year = 2100
+    var leap = year%400 == 0 || (year%4 == 0 && year%100 != 0)
+    if leap {
+    fmt.Println("Look before you leap!")
+    } else {
+        fmt.Println("Keep your feet on the ground.")
+    }
+
+    //QC 3.2 answer Which is greater, an “apple” or a “banana”?
+    fmt.Println("apple" > "banana")
+}
