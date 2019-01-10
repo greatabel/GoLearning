c6530dcc3a50ab906b1a4cd22d4e3950175d6620
greatabel <myreceiver2for2github@gmail.com>
2019-01-10 13:06:14 +0800

    1short_declaration.go

---
+++ 00Get Programming with Go/03LOOPS_AND_BRANCHES/04Variable_Scope/1short_declaration.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+
+)
+
+func main() {
+    // The following two lines are equivalent:
+    var count = 110
+    count0 := 110
+    fmt.Println(count, count0)  
+    for count = 10; count > 0; count -- {
+        fmt.Println(count)
+    }
+
+    fmt.Println(count, count0)
+    
+}
