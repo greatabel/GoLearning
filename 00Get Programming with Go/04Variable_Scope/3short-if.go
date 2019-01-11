ee6cdae1d9ffdeee79c478bcd9b4b2664f902932
greatabel <myreceiver2for2github@gmail.com>
2019-01-11 23:39:37 +0800

    make rand seed

--- 00Get Programming with Go/04Variable_Scope/3short-if.go
+++ 00Get Programming with Go/04Variable_Scope/3short-if.go
@@ -3,9 +3,12 @@ package main
 import (
     "fmt"
     "math/rand"
+    "time"
 )
 
 func main() {
+    rand.Seed(time.Now().UTC().UnixNano())
+    
     if num := rand.Intn(3); num == 0 {
         fmt.Println("Space Adventure")
     } else if num == 1 {
