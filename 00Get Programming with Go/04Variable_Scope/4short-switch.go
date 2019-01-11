393fe822bd35f3e83d8b0afba81a5bacc6e9f84f
greatabel <myreceiver2for2github@gmail.com>
2019-01-11 23:42:18 +0800

    4short-switch.go

--- 00Get Programming with Go/04Variable_Scope/4short-switch.go
+++ 00Get Programming with Go/04Variable_Scope/4short-switch.go
@@ -10,12 +10,17 @@ func main() {
     //https://stackoverflow.com/questions/12321133/golang-random-number-generator-how-to-seed-properly
     rand.Seed(time.Now().UTC().UnixNano())
 
-    if num := rand.Intn(3); num == 0 {
-        fmt.Println("Space Adventure")
-    } else if num == 1 {
-        fmt.Println("SpaceX")
-    } else {
-        fmt.Println("Virgin Galactic")
+    switch num := rand.Intn(10); num {
+    case 0:
+     fmt.Println("Space Adventures")
+    case 1:
+     fmt.Println("SpaceX")
+    case 2:
+     fmt.Println("Virgin Galactic")
+    default:
+     fmt.Println("Random spaceline #", num)
     }
+
+
     
 }
