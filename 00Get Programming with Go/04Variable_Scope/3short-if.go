895f821d29aa8c863604d2d4e201e96eb862833c
greatabel <myreceiver2for2github@gmail.com>
2019-01-11 23:38:54 +0800

    3short-if.go

---
+++ 00Get Programming with Go/04Variable_Scope/3short-if.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+)
+
+func main() {
+    if num := rand.Intn(3); num == 0 {
+        fmt.Println("Space Adventure")
+    } else if num == 1 {
+        fmt.Println("SpaceX")
+    } else {
+        fmt.Println("Virgin Galactic")
+    }
+    
+}
