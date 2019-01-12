1347f11ae9978d577de2a50686c53acc4db0ac29
greatabel <myreceiver2for2github@gmail.com>
2019-01-12 23:00:59 +0800

    0Declaring-floating-point.go

---
+++ 00Get Programming with Go/06REAL NUMBERS/0Declaring-floating-point.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+
+)
+
+var era = "AD"
+
+func main() {
+
+    days := 365.2425
+    var days1 = 365.2425
+    var days2 float64 = 365.2425
+    fmt.Println(days, days1, days2)
+
+
+    
+}
