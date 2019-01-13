0fa9eaed864a3b4b7989fbb6cbb30f530111afec
greatabel <myreceiver2for2github@gmail.com>
2019-01-13 23:49:13 +0800

    specify a floating-poin

--- 00Get Programming with Go/06REAL NUMBERS/0Declaring-floating-point.go
+++ 00Get Programming with Go/06REAL NUMBERS/0Declaring-floating-point.go
@@ -13,7 +13,10 @@ func main() {
     var days1 = 365.2425
     var days2 float64 = 365.2425
     fmt.Println(days, days1, days2)
+    // If you initialize a variable with a whole number, 
+    // Go won’t know you want floating-point unless you explicitly specify a floating-point type:
 
-
+    var answer float64 = 42
+    fmt.Println(answer)
     
 }
