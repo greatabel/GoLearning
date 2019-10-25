411c78f23961ae00734c8146b8f0c28c60ff4d9a
greatabel <myreceiver2for2github@gmail.com>
2019-10-25 19:59:02 +0800

    worldcount.go

--- src/01Go_In_Action/ch3Package_toolchain/wordcount/wordcount.go
+++ src/01Go_In_Action/ch3Package_toolchain/wordcount/wordcount.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+    "fmt"
+    "io/ioutil"
+    "os"
 
+    "01Go_In_Action/ch3Package_toolchain/words"
 )
 
 func main() {
+    filename := os.Args[1]
 
+    contents, err := ioutil.ReadFile(filename)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    text := string(contents)
+
+    count := words.CountWords(text)
+    fmt.Printf("There are %d words in your text. \n", count)
 }
