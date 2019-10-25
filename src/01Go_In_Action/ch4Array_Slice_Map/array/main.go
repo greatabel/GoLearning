f6c6499d48de1dba3abc8bc6776032a216350b4b
greatabel <myreceiver2for2github@gmail.com>
2019-10-25 20:27:46 +0800

    main.go

---
+++ src/01Go_In_Action/ch4Array_Slice_Map/array/main.go
@@ -0,0 +1,15 @@
+// Sample program to show how to show you how to briefly work with io.
+package main
+
+import (
+    "fmt"
+
+
+)
+
+// main is the entry point for the application.
+func main() {
+    // var array [5]int
+    array := [5]int{10, 20, 30, 40, 50}
+    fmt.Printf("There are %d words in your text. \n", array[0])
+}
