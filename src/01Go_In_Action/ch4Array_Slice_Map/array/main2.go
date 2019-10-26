6365111f2ea2fc92f988c479e91e54570a6abbca
greatabel <myreceiver2for2github@gmail.com>
2019-10-26 20:11:00 +0800

    point string array 赋值

--- src/01Go_In_Action/ch4Array_Slice_Map/array/main2.go
+++ src/01Go_In_Action/ch4Array_Slice_Map/array/main2.go
@@ -17,5 +17,13 @@ func main() {
     array1 = array2
     fmt.Printf("There are %s %s %s \n", array1[0], array1[1], array1[2])
 
-  
+    // 指针数组赋值给另一个
+    var array3 [3]*string
+
+    array4 := [3]*string{new(string), new(string), new(string)}
+    *array4[0] = "Red"
+    *array4[1] = "Green"
+    *array4[2] = "Blue"
+    array3 = array4
+    fmt.Printf("There are %s %s %s \n", *array3[0], *array3[1], *array3[2])
 }
