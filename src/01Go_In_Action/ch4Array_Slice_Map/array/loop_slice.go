92d7713d34b24adf13bfde55e10fc0ca40ffb95e
greatabel <myreceiver2for2github@gmail.com>
2019-10-27 16:22:40 +0800

    _ ignore index in range

--- src/01Go_In_Action/ch4Array_Slice_Map/array/loop_slice.go
+++ src/01Go_In_Action/ch4Array_Slice_Map/array/loop_slice.go
@@ -11,10 +11,15 @@ func main() {
     for index, value := range slice {
         fmt.Printf("Index: %d Value: %d\n", index, value)
     }
-    
+
     // 迭代每个元素，并显示值和地址
     for index, value := range slice {
     fmt.Printf("Value: %d Value-Addr: %X ElemAddr: %X\n", value, &value, &slice[index])
+    }
+
+    //使用空白标识符 （下划线）忽略索引值
+    for _, value := range slice {
+        fmt.Printf("# Value: %d\n", value)
     }
 
 
