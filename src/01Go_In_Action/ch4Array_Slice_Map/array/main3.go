fe80ee399bb97b8593d7112e5c47a94f5dfcfc49
greatabel <myreceiver2for2github@gmail.com>
2019-10-27 16:15:45 +0800

    slice

--- src/01Go_In_Action/ch4Array_Slice_Map/array/main3.go
+++ src/01Go_In_Action/ch4Array_Slice_Map/array/main3.go
@@ -11,4 +11,16 @@ func main() {
 
     array := [4][2]int{{10,11}, {20,21}, {30, 31}, {40, 41}}
     fmt.Printf("There are %d %d \n",array[0][0], array[0][1])
+
+    // 不需要指定[]的值
+    slice0 := []string{"Red", "Blue", "Green", "Yellow", "Pink"}
+    //创建一个整形切片,长度和容量都是3个元素
+    slice1 := []int{10, 20, 30}
+    newslice := slice1[1:2]
+
+    //使用空字符串初始化第100个元素
+    slice2 := []string{99: ""}
+    fmt.Printf("There are %s %d %d %s\n",slice0[0], slice1[0], newslice[0], slice2[0])
+
+
 }
